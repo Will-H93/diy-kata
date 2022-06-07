6670619ea0c54a97b916ba4ee9cab02025d6a9fa
Will Hodgkinson <willhodgkinson5@gmail.com>
2022-06-07 21:33:36 +0100

    Kata 2 Complete

--- test/kata2.booleanToWord.test.js
+++ test/kata2.booleanToWord.test.js
@@ -1,5 +1,10 @@
 const { booleanToWord } = require("../src");
 
 describe("booleanToWord", () => {
-  // how do we create specs again???
+  it("returns yes when passed True", () => {
+    expect(booleanToWord(true)).toEqual("Yes");
+  });
+  it("returns no when passed false", () => {
+    expect(booleanToWord(false)).toEqual("No");
+  });
 });
