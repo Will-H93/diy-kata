0703e4edbbcd6c5b29e244fb8168c0f5fb4703cb
Will Hodgkinson <willhodgkinson5@gmail.com>
2022-06-12 19:12:54 +0100

    Completed Reverse Digits

--- src/kata3.numberToReversedDigits.js
+++ src/kata3.numberToReversedDigits.js
@@ -1,3 +1,8 @@
-const numberToReversedDigits = number => {};
+const numberToReversedDigits = number => {
+  const myArray = [];
+  myArray.push(number);
+  myArray.reverse();
+  return myArray;
+};
 
 module.exports = numberToReversedDigits;
