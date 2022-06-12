105145c33cd1bcf483b822e8016dc1212cccbba8
Will Hodgkinson <willhodgkinson5@gmail.com>
2022-06-12 20:20:04 +0100

    Completed and tested Kata4

--- src/kata4.humanCatDogYears.js
+++ src/kata4.humanCatDogYears.js
@@ -1,3 +1,24 @@
-const humanCatDogYears = number => {};
+const humanCatDogYears = number => {
+  const newArray = [number];
+  let catYears = "";
+  let dogYears = "";
+  let year = "";
+  if (number === 1) {
+    catYears = 15;
+    dogYears = 15;
+  } else if (number === 2) {
+    catYears = 15 + 9;
+    dogYears = 15 + 9;
+  } else if (number > 2) {
+    year = number - 2;
+    catYears = 15 + 9 + (year * 4);
+    dogYears = 15 + 9 + (year * 5);
+  } else {
+    catYears = 0;
+    dogYears = 0;
+  }
+  newArray.push(catYears, dogYears);
+  return newArray;
+};
 
 module.exports = humanCatDogYears;
