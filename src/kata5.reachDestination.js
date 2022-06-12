a981c6ac0e68ea1281725a316dd7e66c4cf8a656
Will Hodgkinson <willhodgkinson5@gmail.com>
2022-06-12 21:09:25 +0100

    Kata 5 completed and tested

--- src/kata5.reachDestination.js
+++ src/kata5.reachDestination.js
@@ -1,3 +1,7 @@
-const reachDestination = (distance, speed) => {};
+const reachDestination = (distance, speed) => {
+  const time = Math.round((distance / speed) * 2) / 2;
+  const string = `I should be there in ${time} hours.`;
+  return string;
+};
 
 module.exports = reachDestination;
