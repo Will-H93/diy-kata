7aee977be579d9c7f87db33af5a2063ae99d9d49
Will Hodgkinson <willhodgkinson5@gmail.com>
2022-06-14 21:09:25 +0100

    Kata 7 complete and tested

--- src/kata7.getEmployerRole.js
+++ src/kata7.getEmployerRole.js
@@ -1,3 +1,7 @@
-const getEmployerRole = (employeeName, employees) => {};
+const getEmployerRole = (employeeName, employees) => {
+  const find = employees.find(staff => staff.name === employeeName);
+  const jobTitle = find.role;
+  return jobTitle;
+};
 
 module.exports = getEmployerRole;
